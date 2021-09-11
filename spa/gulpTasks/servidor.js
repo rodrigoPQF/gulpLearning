ec112c0df9f80118439edfe1199d8f069710d627
rodrigoPQF <rodrigoPQF@gmail.com>
2021-09-11 18:12:15 -0300

    Gulp end

--- spa/gulpTasks/servidor.js
+++ spa/gulpTasks/servidor.js
@@ -1,10 +1,22 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
+
+
+
+
 
 function monitorarArquivos(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')())
     return cb()
 }
 function servidor(cb) {
-    return cb()
+    return gulp.src('build')
+    .pipe(webserver({
+        port:8080,
+        open: true,
+        livereload: true
+    }))
 }
 
 module.exports = {
